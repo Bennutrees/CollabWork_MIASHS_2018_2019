dfe8eed4c96bf01084c1bae3fd7381836c1b6426
Bennutrees <benjamin.poulet42@hotmail.fr>
2018-12-15 18:24:35 +0100

    Ajout de ChalPawn.isVulnerable

--- Bagh Chal/src/baghchal/ChalPawn.java
+++ Bagh Chal/src/baghchal/ChalPawn.java
@@ -1,21 +1,44 @@
 package baghchal;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 public class ChalPawn extends AbstractPawn {
 	
+	private boolean isVulnerable;
+	
 	//Constructor
 	public ChalPawn(int x, int y, Board board) {
 		super(x, y, board);
+		this.isVulnerable = this.calculateVulnerability(board);
 	}
 
 	//Methods
 	public String toString(){
 		return "c";
 	}
-
+	
+	private boolean calculateVulnerability(Board board) {
+		HashMap<Square, AbstractPawn> pawnsMap = board.getPawnsMap();
+        Set<Square> squaresOnMap = pawnsMap.keySet();
+        for (Square currentSquare : squaresOnMap) {
+        	boolean isAroundThisSquare = (Math.abs(currentSquare.getPosition().getX() - this.getPosition().getX()) == 1)
+        									|| (Math.abs(currentSquare.getPosition().getY() - this.getPosition().getY()) == 1);
+        	if (isAroundThisSquare) {
+        		AbstractPawn currentPawn = pawnsMap.get(currentSquare);
+        		if (currentPawn instanceof BaghPawn) {
+					return ((BaghPawn) currentPawn).possibleEatMoves() != null;
+				}
+        	}
+        }
+        return false;
+	}
+	
+	public boolean getIsVulnerable() {
+		return this.isVulnerable;
+	}
 	@Override
 	public List<Move> allPawnPossibleMoves() {
 		List<Move> possibleMoves = new ArrayList<Move>();
@@ -28,9 +51,9 @@ public class ChalPawn extends AbstractPawn {
 		if(board.getNbChalsToPlace() > 0) {
 	        Set<Square> squaresOnMap = board.getPawnsMap().keySet();
 	        
-	        for (Square actualSquare : squaresOnMap) {
-	        	if (actualSquare.getIsAvailable()) 
-	        		possibleActions.add(new Move(actualSquare.getPosition(), board));
+	        for (Square currentSquare : squaresOnMap) {
+	        	if (currentSquare.getIsAvailable()) 
+	        		possibleActions.add(new Move(currentSquare.getPosition(), board));
 	        }
 		}
 		else {
